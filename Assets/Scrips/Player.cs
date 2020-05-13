7338d137b0fb226d9bc2e00d4cd02044a98fcf4b
ElGutiz <54872438+ElGutiz@users.noreply.github.com>
2020-05-12 19:02:18 -0600

    First Push

---
+++ Assets/Scrips/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int speed = 10;
+
+    public Rigidbody rb;
+
+    private bool sphereIsOnTheGround = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float moveVertical = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+        if (Input.GetKey(KeyCode.Space) && sphereIsOnTheGround)
+        {
+            rb.AddForce(new Vector3(0, 5, 0), ForceMode.Impulse);
+            sphereIsOnTheGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            sphereIsOnTheGround = true;
+        }
+    }
+}
